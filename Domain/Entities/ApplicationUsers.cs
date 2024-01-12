9ddb5d348edfeae93cc4fab2e5e3a49e342431cd
Amit Singh <np4652@gmail.com>
2024-01-12 16:37:39 +0530

    d

--- Domain/Entities/ApplicationUsers.cs
+++ Domain/Entities/ApplicationUsers.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Entities
+﻿namespace Domain.Entities
 {
-    internal class ApplicationUsers
+    public class ApplicationUsers
     {
+        public string FirstName { get; set; }
+        public bool IsActive { get; set; }
+        public string GAuthPin { get; set; }
+        public string Gender { get; set; }
+        public string MeddleName { get; set; }
+        public string LastName { get; set; }
+        public string AlternateNumber { get; set; }
+        public string WhatsAppNumber { get; set; }
+        public string Address { get; set; }
+        public int PostalCode { get; set; }
+        public ApplicationRoles? RoleId { get; set; }
     }
 }
---
+++ WEBAPI/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Interface;
+using Domain.Service;
+using Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace WEBAPI.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void RegisterService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IContext, Context>();
+            services.AddAuthentication(option =>
+            {
+                option = new Microsoft.AspNetCore.Authentication.AuthenticationOptions
+                {
+                    DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme,
+                    DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme,
+                    DefaultScheme = JwtBearerDefaults.AuthenticationScheme
+                };
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secretkey"]))
+                };
+            }).AddCookie("Cookies");
+            services.AddAuthorization();
+        }
+    }
+}
